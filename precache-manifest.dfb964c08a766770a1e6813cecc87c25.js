8529a3f8c3ddcc2d296584476bcdb5bcb4f49fe0
Abdullahi Aliyu <abbaxee2@gmail.com>
2019-01-31 09:44:27 +0100

    Updates

---
+++ precache-manifest.dfb964c08a766770a1e6813cecc87c25.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3fad880beee2d67cab3cc3e0ca67bca",
+    "url": "/shoppers/static/media/person_3.b3fad880.jpg"
+  },
+  {
+    "revision": "f17b39ab9aa457ae8d50",
+    "url": "/shoppers/static/css/main.691332df.chunk.css"
+  },
+  {
+    "revision": "302fbc0aa5dcec40d385772d64baad2c",
+    "url": "/shoppers/static/media/icomoon.302fbc0a.svg"
+  },
+  {
+    "revision": "ccaf9335295d9fca8196",
+    "url": "/shoppers/static/js/1.ccaf9335.chunk.js"
+  },
+  {
+    "revision": "3c9934308fd4c8ebb351",
+    "url": "/shoppers/static/js/runtime~main.3c993430.js"
+  },
+  {
+    "revision": "f43ec3dc5f4a2846391a5a62bc412e44",
+    "url": "/shoppers/static/media/blog_1.f43ec3dc.jpg"
+  },
+  {
+    "revision": "89166399ac8e1869ada5c1935ee3e915",
+    "url": "/shoppers/static/media/hero_1.89166399.jpg"
+  },
+  {
+    "revision": "7eee32953a35c7ab0e07145edfdb0cc2",
+    "url": "/shoppers/static/media/person_1.7eee3295.jpg"
+  },
+  {
+    "revision": "545486c358d7992328c4b1b8feb59433",
+    "url": "/shoppers/static/media/person_2.545486c3.jpg"
+  },
+  {
+    "revision": "f17b39ab9aa457ae8d50",
+    "url": "/shoppers/static/js/main.f17b39ab.chunk.js"
+  },
+  {
+    "revision": "ccd8da51b1c9f317975f0a5ef3273196",
+    "url": "/shoppers/static/media/person_4.ccd8da51.jpg"
+  },
+  {
+    "revision": "4da67b10dff4d8f3e616c1e5e4e4c9db",
+    "url": "/shoppers/static/media/women.4da67b10.jpg"
+  },
+  {
+    "revision": "9287915455b51fbd1594edc8a9fd5c8a",
+    "url": "/shoppers/static/media/children.92879154.jpg"
+  },
+  {
+    "revision": "125f60089fb82e2915471e82e02af813",
+    "url": "/shoppers/static/media/men.125f6008.jpg"
+  },
+  {
+    "revision": "bab8cf81a530d89b36f24a80649c5e25",
+    "url": "/shoppers/static/media/icomoon.bab8cf81.ttf"
+  },
+  {
+    "revision": "62f33c03d096cdc058166bd6fbf7e4d0",
+    "url": "/shoppers/static/media/icomoon.62f33c03.woff"
+  },
+  {
+    "revision": "2cb8dc8186e8827cca3b6beebe4ef1c6",
+    "url": "/shoppers/static/media/icomoon.2cb8dc81.eot"
+  },
+  {
+    "revision": "ccaf9335295d9fca8196",
+    "url": "/shoppers/static/css/1.3662e1d2.chunk.css"
+  },
+  {
+    "revision": "f8e8e92e7021b2055b999c7b5059bcb4",
+    "url": "/shoppers/index.html"
+  }
+];
