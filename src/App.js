30b13666842e1f50e691cea0a92bccdb858a7e21
Abdullahi Aliyu <abbaxee2@gmail.com>
2019-01-31 23:48:05 +0100

    fix: routing

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
   render() {  
     return (
       <Provider store={store}>
-        <BrowserRouter history={history}>
+        <BrowserRouter basename="/shoppers" history={history}>
           <ScrollToTop>
             <Header />
             <Breadcrumb />
